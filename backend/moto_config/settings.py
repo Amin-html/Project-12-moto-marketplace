19f39fd495f74a9782498c068ab7e604053f6246
Amin-html <adilov.amin.rx7.a86@gmail.com>
2026-08-11 15:52:17 +0600

    chore(deploy): add gunicorn and production docker-compose override

--- backend/moto_config/settings.py
+++ backend/moto_config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 
 
 # Application definition
@@ -176,3 +176,4 @@ CACHES = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'noreply@moto-marketplace.local'
+
